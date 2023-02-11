ae07eca472317fecb0b93a03a6b427294fd2535c
FAQIHdimas2005 <123342985+FAQIHdimas2005@users.noreply.github.com>
2023-02-11 11:12:41 +0700

    Add files via upload

---
+++ soal7.ts
@@ -0,0 +1,23 @@
+// no 7
+const fruits:string[]= ["Banana", "Orange", "Apple", "Mango"] console.log(fruits.join('-'));
+// <>
+// no 1
+
+for (let y = 0; y <= 10; y++) {   for (let x = 0; x <= 10; x++)     if (       y == x ||       -x + y == 0 ||       -x + y == 1 ||       -x + y == 2 ||       -x + y == 3 ||       -x + y == 4 ||       -x + y == 5 ||       -x + y == 6 ||       -x + y == 7 ||       -x + y == 8 ||       -x + y == 9 ||       -x + y == 10     ) {       document.write(" * ");     } else {       document.write(" _ ");     }   document.write("<br>"); }
+// <>
+// no 4
+let animals: string[] = ['lion','elephant','squirrel','eagle','zebra','tiger'] animals.slice(0,5) console.log(animals);
+// <>
+// no 5
+pakai method array, sort
+
+// <>
+// no 6
+let mobile:string[] = ['A Faqih', 'David', 'El Ghifari', 'Fajar','Faqih'];  let backend:string[] = ['Arga', 'Ramdhan', 'Luqman', 'Andaru','Aldimas'];  let gabung=mobile.concat(backend)  console.log(gabung);
+// <>
+// no 7
+const fruits:string[]= ["Banana", "Orange", "Apple", "Mango"] console.log(fruits.join('-'));
+// <>8
+let hewan: string[] = ['lion','elephant','squirrel','eagle','zebra','tiger']
+hewan.slice(2,3,4,)
+console.log(hewan);
